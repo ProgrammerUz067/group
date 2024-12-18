45c746a3197dd5c558d0d4519a49cf0343e3424e
Diyor <baxacap@gmail.com>
2024-12-18 12:35:03 +0500

    mainPage

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,61 +1,47 @@
-import React from 'react';
-import style from './MainPage.module.scss';
-import Sphere from '../Nova/Sphere';
-import Button from '../Button/Button';
-import { useTranslation } from 'react-i18next';
-import Reveal from '../Reveal/Reveal';
-import { useEffect, useState } from 'preact/hooks';
+import React from "react";
+import style from "./MainPage.module.scss";
+import Sphere from "../Nova/Sphere";
+import Button from "../Button/Button";
+import { useTranslation } from "react-i18next";
+import Reveal from "../Reveal/Reveal";
+import { useEffect, useState } from "preact/hooks";
+import img from "../../../public/main.png"
 
 export default function MainPage() {
   const { t } = useTranslation();
-  const [showMain, setShowMain] = useState(false)
+  const [showMain, setShowMain] = useState(false);
   const handleScroll = () => {
     const scrollTop = window.pageYOffset;
     console.log(scrollTop);
     if (scrollTop >= 900) {
-      setShowMain(true) 
-      
+      setShowMain(true);
     } else {
-      setShowMain(false)
-      
+      setShowMain(false);
     }
   };
-  useEffect(()=>{
-    window.addEventListener('scroll', handleScroll)
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
 
-    return () =>{
-      window.removeEventListener('scroll', handleScroll)
-    }
-  },[])
- 
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={`${style.mainPage} ${showMain ? 'opacity-[0%]' : ''}`}>
-      <div className="w-[60%] z-10">
-        {' '}
+    <div className={`${style.mainPage} ${showMain ? "opacity-[0%]" : ""}`}>
+      <div className="w-[50%] z-10">
+        {" "}
         <Reveal delay="8.8">
-          <h1 className={style.title}>
-            {' '}
-            <div className={style.mainTitle}>
-              <span>UZ</span> <div>IT</div>{' '}
-            </div>{' '}
-            <p>GROUP</p>{' '}
-          </h1>
+          <p className={style.title}>{t("main.titleGreen")} <span>{t("main.titleWhite")}</span></p>
         </Reveal>
         <Reveal delay="8.8">
-          <p className={style.subtitle}>{t('main.main-text')}</p>
+          <p className={style.subtitle}>{t("main.main-text")}</p>
         </Reveal>
-        <div className="mt-[50px]">
-          {/* <Button width={280} height={71} font={25}>
-            Подробнее
-          </Button> */}
-
-          <Button width={291} height={71} font={25}>
-            {t('main.button')}
-          </Button>
-        </div>
+        
       </div>
-      <Sphere />
-      {/* <Sphere2 /> */}
+      <div className="w-[50%] z-10 mt-[100px] ml-[100px]">
+        <img src={img} alt="img" />
+      </div>
     </div>
   );
 }
