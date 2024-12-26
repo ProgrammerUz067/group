04d7132dc3810d591a4b1b4a8e30f2ff2cda68dd
Diyor <baxacap@gmail.com>
2024-12-26 13:51:20 +0500

    code

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,42 +1,18 @@
 import React from "react";
 import style from "./MainPage.module.scss";
-import Sphere from "../Nova/Sphere";
-import Button from "../Button/Button";
 import { useTranslation } from "react-i18next";
 import Reveal from "../Reveal/Reveal";
-import { useEffect, useState } from "preact/hooks";
 import img from "../../../public/main.png"
 
 export default function MainPage() {
   const { t } = useTranslation();
-  const [showMain, setShowMain] = useState(false);
-  const handleScroll = () => {
-    const scrollTop = window.pageYOffset;
-    console.log(scrollTop);
-    if (scrollTop >= 900) {
-      setShowMain(true);
-    } else {
-      setShowMain(false);
-    }
-  };
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   return (
     <div className={`${style.mainPage}`}>
       <div className="w-[50%] z-10">
         {" "}
-        <Reveal delay="8.8">
           <p className={style.title}>{t("main.titleGreen")} <span>{t("main.titleWhite")}</span></p>
-        </Reveal>
-        <Reveal delay="8.8">
           <p className={style.subtitle}>{t("main.main-text")}</p>
-        </Reveal>
         
       </div>
       <div className="w-[50%] z-10 mt-[100px] ml-[100px]">
