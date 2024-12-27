2a4a696907cc03853e87c4a895aa56d2e7f108b1
Diyor <baxacap@gmail.com>
2024-12-27 15:07:48 +0500

    planshet main

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -12,16 +12,34 @@ export default function Contacts() {
     <section id="contacts" className={styles.contacts}>
       <h1 className={styles.title}>{t("map.title")}</h1>
 
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3070.2764570900745!2d66.98631867601168!3d39.68848767156592!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f4d191dab2d0e2f%3A0xc760f3162c836017!2sUZIT%20Group!5e0!3m2!1sru!2s!4v1734626534963!5m2!1sru!2s"
-        width="1400"
-        height="300"
-        style="border:0;"
-        allowfullscreen=""
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-        className={styles.map}
-      ></iframe>
+      <div style="position:relative;overflow:hidden;">
+        <a
+          href="https://yandex.uz/maps/org/uzit_group/41170032874/?utm_medium=mapframe&utm_source=maps"
+          style="color:#eee;font-size:12px;position:absolute;top:0px;"
+        >
+          Uzit Group
+        </a>
+        <a
+          href="https://yandex.uz/maps/10334/samarkand/category/it_company/184106174/?utm_medium=mapframe&utm_source=maps"
+          style="color:#eee;font-size:12px;position:absolute;top:14px;"
+        >
+          IT-компания в Самарканде
+        </a>
+        <a
+          href="https://yandex.uz/maps/10334/samarkand/category/marketing_services/184105524/?utm_medium=mapframe&utm_source=maps"
+          style="color:#eee;font-size:12px;position:absolute;top:28px;"
+        >
+          Маркетинговые услуги в Самарканде
+        </a>
+        <iframe
+          src="https://yandex.uz/map-widget/v1/?ll=66.989284%2C39.688934&mode=search&oid=41170032874&ol=biz&z=16.73"
+          width="1500"
+          height="250"
+          frameborder="1"
+          allowfullscreen="true"
+          style="position:relative;"
+        ></iframe>
+      </div>
     </section>
   );
 }
